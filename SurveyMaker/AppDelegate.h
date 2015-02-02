0130a90fad25ca898f5f238f738152f6219a6a61
Cameron Klein <cameron.klein@gmail.com>
2015-02-02 13:39:53 -0800

    Initial Commit

---
+++ SurveyMaker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SurveyMaker
+//
+//  Created by Cameron Klein on 2/2/15.
+//  Copyright (c) 2015 Cameron Klein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SurveyMaker/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SurveyMaker
+//
+//  Created by Cameron Klein on 2/2/15.
+//  Copyright (c) 2015 Cameron Klein. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
